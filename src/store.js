cad049a301d04564ecad244a8e4a3a1dcb272bb9
andrew.mandruk@runsel.com <andrew.mandruk@runsel.com>
2019-08-29 23:50:02 +0300

    Api key add to store

--- src/store.js
+++ src/store.js
@@ -1,16 +1,47 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
+import VuexPersist from 'vuex-persist';
+
+const vuexPersist = new VuexPersist({
+  key: 'weather',
+  storage: window.localStorage,
+});
+
+const base = axios.create({
+  baseURL: 'https://api.openweathermap.org/data/2.5',
+});
+
+Vue.prototype.$http = base;
+
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    apiKey: '10d07494e770c41f4af2a455d92ee8f5',
+    cities: {},
+  },
+  getters: {
 
   },
   mutations: {
 
   },
   actions: {
-
+    GET_WEATHER: async (context, payload) => {
+      base.get('/weather', {
+        params: {
+          APPID: context.state.apiKey,
+          q: payload,
+        },
+      }).then((response) => {
+        console.log(response.data)
+      })
+        .catch((e) => {
+          console.error(e)
+        })
+    },
   },
+  plugins: [vuexPersist.plugin],
 });
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,22 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+  computed: {
+    
+  },
+  created() {
+    this.GET_WEATHER('London')
+  },
+  methods: {
+    ...mapActions(['GET_WEATHER'])
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
